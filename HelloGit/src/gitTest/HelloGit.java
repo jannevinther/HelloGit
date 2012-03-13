832e13cef2c1b733ccbfe34feaa4b7280eef5707
jannevinther <jannevinther@sol.dk>
2012-03-13 13:56:54 +0100

    This is the next commit

---
+++ HelloGit/src/gitTest/HelloGit.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+import javax.swing.JOptionPane;
+
+
+public class HelloGit {
+	public static void main(String[] args) {
+		String s = "Hello Git, Welcome to the Java World";
+		JOptionPane.showMessageDialog(null, s);
+	}
+
+}
+
